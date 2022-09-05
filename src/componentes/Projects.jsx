7e19e1df9b66cc72b4bd683ce1568cd88ee7677e
MAS234 <miguel.martinez.angel.salazar@gmail.com>
2022-09-05 18:34:38 -0300

    cambios2

--- src/componentes/Projects.jsx
+++ src/componentes/Projects.jsx
@@ -6,7 +6,7 @@ export default function Projects() {
   return (
     
     <div className='w-full h-screen bg-black flex justify-center items-center flex-col '>
-      <h2 className='mt-[50%] text-2xl text-white md:text-4xl text-semibold md:p-5 ' id='proyectos'>Mis Proyectos</h2>
+      <h2 className='mt-[50%] md:mt-0 text-2xl text-white md:text-4xl text-semibold md:p-5 ' id='proyectos'>Mis Proyectos</h2>
       <Carrusel/>
     </div>
   )
