3f3e2873da252fd44520474bccfc19837b47d075
MAS234 <miguel.martinez.angel.salazar@gmail.com>
2022-09-05 15:44:14 -0300

    cambios

--- src/componentes/About.jsx
+++ src/componentes/About.jsx
@@ -11,14 +11,14 @@ export default function About() {
 
       <div className=' text-white m-auto w-[50%] h-[400px] md:text-left text-center mb-10'>
 
-        <h1 className='text-2xl md:text-3xl m-auto p-[10px] ' id='sobremi'>Sobre Mi</h1>
-        <div className='m-auto mt-[5px] md:mt-[15px] text-sm md:text-lg p-[5px]'>
+        <h1 className='text-2xl md:text-3xl m-auto  p-[10px] ' id='sobremi'>Sobre Mi</h1>
+        <div className='m-auto mt-[5px] md:mt-[15px] text-md md:text-lg p-[5px]'>
           <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, exercitationem? Voluptates iure culpa odio debitis iste sequi non? Eaque fugit odit, soluta quod laboriosam sint hic rem ad accusamus voluptates.</p>
         </div>
 
         <h2 className='m-auto mt-[5px] md:mt-[20px] text-2xl md:text-3xl'>Lenguajes</h2>
 
-        <div className='flex justify-evenly items-center m-5'>
+        <div className='flex justify-evenly items-center m-5 gap-4'>
           <img className='mt-[20px] md:mt-[40px] h-[40px] md:h-[50px] hover:scale-110 duration-300' src={Css} alt="css" />
           <img className='mt-[20px] md:mt-[40px] h-[40px] md:h-[50px] hover:scale-110 duration-300' src={Html} alt="Html" />
           <img className='mt-[20px] md:mt-[40px] h-[40px] md:h-[50px] hover:scale-110 duration-300' src={Js} alt="Js" />
@@ -29,7 +29,7 @@ export default function About() {
       </div>
 
       <div className=' mt-[50px] md:w-auto md:m-auto '>
-         <img src={fotoPerfil} alt="fotoPerfil" className='md:w-[50vh] md:h-[400px]  w-[60%] h-[350px] rounded-sm  hover:scale-110 duration-300 m-auto'/>
+         <img src={fotoPerfil} alt="fotoPerfil" className='md:w-[50vh] md:h-[400px]  w-[70%] h-[350px] rounded-sm  hover:scale-110 duration-300 m-auto'/>
       </div>
     </div>
   )
